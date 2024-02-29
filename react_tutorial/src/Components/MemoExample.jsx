fbacd60075f64cc89d489fec6cee2a09ff85fce8
Brijesh03032001 <sainibrijesh01@gmail.com>
2024-03-01 01:44:51 +0530

    react added plus memo_example

---
+++ react_tutorial/src/Components/MemoExample.jsx
@@ -0,0 +1,26 @@
+import { memo, useState } from 'react';
+
+const EmployeeProfile = memo(function MemoExample({ name, email }) {
+  return (<>
+        <p>Name:{name}</p>
+        <p>Email: {email}</p>
+        </>);
+});
+function MemoExample() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  return (
+    <>
+      <label>
+        Name: <input value={name} onChange={e => setName(e.target.value)} />
+      </label>
+      <label>
+        Email: <input value={email} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <hr/>
+      <EmployeeProfile name={name}/>
+    </>
+  );
+}
+
+export default MemoExample;
